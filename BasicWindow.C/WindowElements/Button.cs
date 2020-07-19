3e3fc2a01cc31921252c8b3871bc2fd079645cb2
Ignacio Tampe <Igtampe@outlook.com>
2020-07-19 01:56:43 -0400

    Moved CloseButton out of Button.cs
    There's no need to do a mess like that

--- BasicWindow.C/WindowElements/Button.cs
+++ BasicWindow.C/WindowElements/Button.cs
@@ -62,23 +62,4 @@ namespace Igtampe.BasicWindows.WindowElements {
         public abstract KeyPressReturn Action();
     }
 
-    /// <summary>Button that signals the window to close when pressed.</summary>
-    public class CloseButton:Button {
-
-        /// <summary>Creates a button that interacted with, will close the window</summary>
-        /// <param name="Parent"></param>
-        /// <param name="Text"></param>
-        /// <param name="BG"></param>
-        /// <param name="FG"></param>
-        /// <param name="HighlightedBG"></param>
-        /// <param name="LeftPos"></param>
-        /// <param name="TopPos"></param>
-        public CloseButton(Window Parent,string Text,ConsoleColor BG,ConsoleColor FG,ConsoleColor HighlightedBG,int LeftPos,int TopPos) : base(Parent,Text,BG,FG,HighlightedBG,LeftPos,TopPos) { }
-
-        /// <summary>Closes the window</summary>
-        /// <returns></returns>
-        public override KeyPressReturn Action() { return KeyPressReturn.CLOSE; }
-    }
-
-
 }
