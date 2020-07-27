0512a79ec497ed1648f8238e06fe571202dfe35e
Ignacio Tampe <Igtampe@outlook.com>
2020-07-26 23:23:04 -0400

    Update Versions/Properties For Release

--- BasicGraphics.C/Properties/AssemblyInfo.cs
+++ BasicGraphics.C/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BasicGraphics.C")]
+[assembly: AssemblyTitle("BasicGraphics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BasicGraphics.C")]
+[assembly: AssemblyProduct("BasicGraphics")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
