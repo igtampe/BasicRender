17b830c8efac1440de76b941ae108ece99c507b9
Ignacio Tampe <Igtampe@outlook.com>
2020-12-18 13:36:51 -0400

    Using the cycler object in Spiner in BasicWindows

--- BasicWindow.C/TickableWindowElements/Spinner.cs
+++ BasicWindow.C/TickableWindowElements/Spinner.cs
@@ -1,17 +1,11 @@
 ﻿using Igtampe.BasicRender;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Igtampe.BasicWindows.TickableWindowElements {
 
     /// <summary>Spinner that "spins" one frame every 250ms</summary>
     public class Spinner:TickableWindowElement {
-
-        private int State = 0;
-        private readonly String[] Frames = { "|","/","-","\\"};
+        private readonly Cycler cycler;
 
         private readonly ConsoleColor BG;
         private readonly ConsoleColor FG;
@@ -23,6 +17,8 @@ namespace Igtampe.BasicWindows.TickableWindowElements {
         /// <param name="LeftPos"></param>
         /// <param name="TopPos"></param>
         public Spinner(Window Parent, ConsoleColor BG, ConsoleColor FG, int LeftPos, int TopPos):base(Parent) {
+            string[] Frames = { "|","/","-","\\" };
+            cycler =new Cycler(Frames);
             this.LeftPos = LeftPos;
             this.TopPos = TopPos;
             this.BG = BG;
@@ -30,11 +26,7 @@ namespace Igtampe.BasicWindows.TickableWindowElements {
         }
 
         /// <summary>Draws the spinner, and increments its state by one.</summary>
-        public override void DrawElement() {
-            Draw.Sprite(Frames[State],BG,FG,LeftPos + Parent.LeftPos,TopPos + Parent.TopPos);
-            State++;
-            if(State == Frames.Length) { State = 0; }
-        }
+        public override void DrawElement() {Draw.Sprite(cycler.Cycle(),BG,FG,LeftPos + Parent.LeftPos,TopPos + Parent.TopPos);}
 
         /// <summary>Draws the element again, spinning one frame</summary>
         /// <returns>Always returns true. Execution is not determined by the spinner.</returns>
