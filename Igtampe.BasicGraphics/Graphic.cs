216ccb845da8ebdf040b5bd0d8616af7ea86dca1
Ignacio Tampe <Igtampe@gmail.com>
2022-03-03 15:45:50 -0400

    Return to abstract

--- Igtampe.BasicGraphics/Graphic.cs
+++ Igtampe.BasicGraphics/Graphic.cs
@@ -1,6 +1,6 @@
 ﻿namespace Igtampe.BasicGraphics {
     /// <summary>Holder for any type of drawable graphic data</summary>
-    public class Graphic {
+    public abstract class Graphic {
 
         /// <summary>Contents of this graphic</summary>
         protected string[] Contents;
@@ -9,7 +9,7 @@
         public string Name { get; set; }
 
         /// <summary>Creates a graphic with specified contents and name</summary>
-        public Graphic(string[] Contents, string Name) {
+        internal Graphic(string[] Contents, string Name) {
             this.Contents = Contents;
             this.Name = Name;
         }
@@ -17,7 +17,7 @@
         /// <summary>Draws the graphic on screen</summary>
         /// <param name="LeftPos"></param>
         /// <param name="TopPos"></param>
-        public virtual void Draw(int LeftPos, int TopPos) => throw new System.NotImplementedException("Graphic should not be used this way! You must set Draw");
+        public abstract void Draw(int LeftPos, int TopPos);
 
         /// <summary>Gets the width of the graphic</summary>
         /// <returns>Length of the first line</returns>
